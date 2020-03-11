ab41f75a534fb4929e86ed192a52c96d25cc29fd
Schouketta <sarah.le@live.fr>
2020-03-11 10:47:50 +0100

    ListView pour mainActivity

--- app/src/main/java/fr/android/moi/projetandroid/MainActivity.java
+++ app/src/main/java/fr/android/moi/projetandroid/MainActivity.java
@@ -3,12 +3,31 @@ package fr.android.moi.projetandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView mListView;
+    private String[] prenoms = new String[]{
+            "Antoine", "Axel", "Cyril", "Ramzi", "Sarah", "Florian",
+            "Nelly", "Sarub", "Lola", "Pichi", "Chipie", "Logan",
+            "Matthieu", "Rossard", "Jacques", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, prenoms);
+        mListView.setAdapter(adapter);
     }
+
+
+
 }
