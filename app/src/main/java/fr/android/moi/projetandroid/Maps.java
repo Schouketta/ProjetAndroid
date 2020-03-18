cd10465f775f4f3e42508832825f31ddaeb45bd8
Sarah <sarahfong@outlook.fr>
2020-03-18 12:18:10 +0100

    Map ok

--- app/src/main/java/fr/android/moi/projetandroid/Maps.java
+++ app/src/main/java/fr/android/moi/projetandroid/Maps.java
@@ -1,15 +1,21 @@
 package fr.android.moi.projetandroid;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import com.google.android.gms.maps.CameraUpdate;
+import android.util.Log;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import fr.android.moi.projetandroid.R;
 
 public class Maps extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,6 +29,7 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -40,7 +47,8 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        //LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(48.866667, 2.333333);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
