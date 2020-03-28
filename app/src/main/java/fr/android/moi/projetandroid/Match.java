c57eea848dbf458910ef90b72db3250d7522e1dd
Schouketta <sarah.le@live.fr>
2020-03-29 00:56:48 +0100

    geoloc ok mais pas propre

--- app/src/main/java/fr/android/moi/projetandroid/Match.java
+++ app/src/main/java/fr/android/moi/projetandroid/Match.java
@@ -19,7 +19,7 @@ public class Match extends AppCompatActivity {
         btnMap = (Button) findViewById(R.id.btnMap);
 
 
-        final Intent intent = new Intent(this, Maps.class);
+        final Intent intent = new Intent(this, Geolocalisation.class);
         btnMap.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(intent);
